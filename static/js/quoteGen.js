74a8e220eef2cca54417f96ae1075ba4d6d4446f
Michael Reisz <michael_reisz@yahoo.com>
2017-04-08 23:00:22 -0400

    Add the ajax call
    Actually add the ajax api call to get the quotes and a start to
    scrolling through them.  Still some bugs in this, but it's getting
    closer.

---
+++ static/js/quoteGen.js
@@ -0,0 +1,57 @@
+
+var quoteArray = [];
+var quoteIndex = 0;
+
+$( document ).ready(function() {
+    getNewQuote();
+});
+
+
+function getNewQuote() {
+  $.ajax({
+      url: "http://api.forismatic.com/api/1.0/",
+      jsonp: "jsonp",
+      dataType: "jsonp",
+      data: {
+        method: "getQuote",
+        lang: "en",
+        format: "jsonp"
+      }
+    })
+    .done(returnQuote)
+    .fail(handleErr);
+};
+
+function returnQuote(response) {
+  quoteArray.push(response);
+  if (quoteArray.length == 1) {
+      displayQuote();
+  }
+  console.log(quoteArray);
+  console.log(quoteIndex);
+};
+
+function handleErr(jqxhr, textStatus, err) {
+  console.log("Request Failed: " + textStatus + ", " + err);
+};
+
+function displayQuote() {
+    $("#quote").text(quoteArray[quoteIndex].quoteText);
+    $("#author").text(quoteArray[quoteIndex].quoteAuthor);
+    console.log(quoteArray[quoteIndex].quoteText);
+};
+
+$("#next-arrow").click(function() {
+    if (quoteIndex < quoteArray.length) {
+        getNewQuote();
+    }
+    quoteIndex++;
+    displayQuote();
+});
+
+$("#prev-arrow").click(function() {
+    if (quoteIndex > 0) {
+        quoteIndex--;
+    }
+    displayQuote();
+})
